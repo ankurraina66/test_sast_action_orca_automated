fd42e1d0f9f47554973a7ec23b86f7ca87381b07
ankurraina66 <65275253+ankurraina66@users.noreply.github.com>
2026-07-14 18:29:22 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-/*
-Copyright 2022, 2026 HCL America, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 import * as core from '@actions/core';
 import * as constants from './constants.js';
 import client from './client.js';
@@ -103,7 +87,7 @@ saclientutil.downloadClient()
     return statusChecker.waitForAnalysis(sastScanId, scaScanId);
 })
 .then((analysisResult) => {
-	if(analysisResult == false) {
+	if(analysisResult === false) {
 		return false;
 	}
 	if(analysisResult) {
@@ -114,7 +98,7 @@ saclientutil.downloadClient()
     return resultProcessor.processScanResults(sastScanId, scaScanId);
 })
 .then(async(results) => {
-	if(results == false){
+	if(results === false){
 		return;
 	}
     if(results) {
