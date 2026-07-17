9ba43deccd147b43b3350ffa9b4c56300f48c6de
ankurraina66 <65275253+ankurraina66@users.noreply.github.com>
2026-07-17 22:06:27 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-/*
-Copyright 2022, 2026 HCL America, Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 import * as core from '@actions/core';
 import * as constants from './constants.js';
 import client from './client.js';
@@ -89,13 +73,16 @@ saclientutil.downloadClient()
 	}
     
     if(process.env.INPUT_WAIT_FOR_ANALYSIS !== 'true') {
-        return;
+        return null;
     }
 
     core.info(constants.WAIT_FOR_ANALYSIS);
     return statusChecker.waitForAnalysis(sastScanId, scaScanId);
 })
 .then((timedOut) => {
+	if(timedOut === null) {
+		return null;
+	}
     if(timedOut) {
         core.warning(constants.ANALYSIS_TIMEOUT);
         return;
@@ -104,6 +91,9 @@ saclientutil.downloadClient()
     return resultProcessor.processScanResults(sastScanId, scaScanId);
 })
 .then(async(results) => {
+	if(results === null) {
+		return;
+	}
     if(results) {
         core.info(results);
 		//Generate markdown + html report
